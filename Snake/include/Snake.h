48e35d3f49b13554da9842e2c464d1870652e141
Dineshs737 <dinesh77saarck@gmail.com>
2024-01-16 18:03:52 +0530

    update SnakeGame added Snake Class

---
+++ Snake/include/Snake.h
@@ -0,0 +1,14 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+class Snake{
+
+private:// private data typessss
+
+
+public://public data typess
+
+};
+
+
+#endif // SNAKE_H
--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,1 +1,27 @@
+#include<SFML/Graphics.hpp>
 
+using namespace sf;
+
+int main()
+{
+    Window window(VideoMode(800,600),"SnakeGame"); // create a object for Window class
+
+    while(window.isOpen()){// created a infinite loop
+
+        Event event; // create  event object for Event class
+
+        while(window.pollEvent(event)) // check the event
+        {
+            if(event.type==Event::Closed) // this function true stop the loop
+
+                window.close();
+
+        }
+
+        /****************** Work the Snake function and work script ****************/
+
+
+    }
+
+    return 0;
+}
